3ef9b8275403952f1bfeac13b1e81cdc1d5c99ed
kestenbok <kristiyan.nedyalkov98@gmail.com>
2024-12-25 03:06:20 +0200

    fix: import paths

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { ConfigService } from '@nestjs/config';
 
-import { configureServer } from './bootstrap/server.bootstrap';
-import { configureGlobals } from './bootstrap/globals.bootstrap';
-import { configureOpenApi } from './bootstrap/openapi.bootstrap';
+import { configureServer } from './bootstrap/server.config';
+import { configureGlobals } from './bootstrap/globals.config';
+import { configureOpenApi } from './bootstrap/openapi.config';
 
 async function bootstrap() {
   const app = await configureServer();
